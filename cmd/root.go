425ba0591f5655f7cdc27e90b9330fe83065f848
radoondas <ondas.radovan@gmail.com>
2019-04-16 17:19:43 -0700

    Support for libbeat v7.0.0.

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,12 @@ package cmd
 import (
 	"github.com/radoondas/safecastbeat/beater"
 
-	cmd "github.com/elastic/beats/libbeat/cmd"
+	"github.com/elastic/beats/libbeat/cmd"
+	"github.com/elastic/beats/libbeat/cmd/instance"
 )
 
 // Name of this beat
 var Name = "safecastbeat"
 
 // RootCmd to handle beats cli
-var RootCmd = cmd.GenRootCmd(Name, "", beater.New)
+var RootCmd = cmd.GenRootCmdWithSettings(beater.New, instance.Settings{Name: Name})
